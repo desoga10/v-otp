cc69a85673d6c22c9a5db279cd0468c410082c55
desoga10 <done2stay@gmail.com>
2023-09-22 19:58:17 +0100

    Initial Commit

--- src/views/PhoneNumber.vue
+++ src/views/PhoneNumber.vue
@@ -1,13 +1,42 @@
-<template lang="">
-  <div>
-    <h2>Hello Number</h2>
-  </div>
+<template>
+  <v-sheet width="300" style="margin-top: 18%;" class="mx-auto ">
+    <v-form @submit.prevent="submitForm">
+      <v-text-field v-model="phoneNumber" label="Phone Number"></v-text-field>
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
-<script>
-export default {
 
+<script>
+import router from "@/router";
+
+import { supabase } from "../supabase";
+
+
+export default {
+  data: () => ({
+    phoneNumber: ''
+  }),
+  methods: {
+    async submitForm() {
+      if (this.phoneNumber.length < 10) {
+        alert('Please enter a valid phone number')
+        return
+      }
+      let { data, error } = await supabase.auth.signInWithOtp({
+        phone: this.phoneNumber,
+      })
+
+      if (data.messageId) {
+        localStorage.setItem('number', this.phoneNumber)
+        router.push({ name: "signin" });
+      }
+
+
+      if (error) {
+        alert(error.msg)
+      }
+    }
+  }
 }
 </script>
-<style lang="">
-
-</style>
